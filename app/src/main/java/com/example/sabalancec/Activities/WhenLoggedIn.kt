67cc30b3a9fd0a7ad6bfd58a8b04e16450d9a025
RYZEN\Ryzen <karlisvia@gmail.com>
2025-03-17 21:50:36 +0200

    Application start

---
+++ app/src/main/java/com/example/sabalancec/Activities/WhenLoggedIn.kt
@@ -0,0 +1,15 @@
+package com.example.sabalancec.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sabalancec.R
+
+class WhenLoggedIn : AppCompatActivity() {
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      // Set the layout for the activity.
+      // For example, if you have a layout named 'activity_when_logged_in.xml':
+      setContentView(R.layout.activity_when_logged_in)
+
+   }
+}
---
+++ app/src/main/java/com/example/sabalancec/Fragments/CatalogueFragment.kt
@@ -0,0 +1,18 @@
+package com.example.sabalancec.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sabalancec.R
+
+
+class CatalogueFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_catalogue, container, false)
+    }
+}
