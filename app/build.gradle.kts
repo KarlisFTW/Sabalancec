c60f5b22b96d2ee6fd3de139073a67f5fc66e40d
pandgit22 <144793362+pandgit22@users.noreply.github.com>
2025-03-26 15:53:50 +0200

    Allergen item detail screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation (libs.retrofit)
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.hbb20:ccp:2.6.0")
     implementation (libs.androidx.core.splashscreen)
